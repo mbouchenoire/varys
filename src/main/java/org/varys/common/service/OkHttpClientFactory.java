1c231be8381c756e5423fb5e05c63d0a09f4bd39
Maxime Bouchenoire <bouchenoire.maxime@gmail.com>
2018-11-22 22:21:54 +0100

    Force UTF-8 encoding of HTTP responses

--- src/main/java/org/varys/common/service/OkHttpClientFactory.java
+++ src/main/java/org/varys/common/service/OkHttpClientFactory.java
@@ -1,9 +1,11 @@
 package org.varys.common.service;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
@@ -13,25 +15,36 @@ public final class OkHttpClientFactory {
         super();
     }
 
+    private static Response loggingInterceptor(Interceptor.Chain chain) throws IOException {
+        final Request request = chain.request();
+        Log.trace(request.toString());
+        final Response response = chain.proceed(chain.request());
+
+        if (response.isSuccessful()) {
+            Log.trace(response.toString());
+        } else {
+            Log.error(response.toString());
+        }
+        return response;
+    }
+
+    private static Response utf8EncodingInterceptor(Interceptor.Chain chain) throws IOException {
+        return chain.proceed(chain.request())
+                .newBuilder()
+                .addHeader("Content-Type", "application/json; charset=utf-8")
+                .build();
+    }
+
     public static OkHttpClient create() {
 
         try {
-            //noinspection deprecation, will change later
+
             return new OkHttpClient().newBuilder()
                     .sslSocketFactory(SSLUtils.createSocketFactory())
                     .hostnameVerifier((s, sslSession) -> true)
-                    .addInterceptor(chain -> {
-                        final Request request = chain.request();
-                        Log.trace(request.toString());
-                        final Response response = chain.proceed(chain.request());
-
-                        if (response.isSuccessful()) {
-                            Log.trace(response.toString());
-                        } else {
-                            Log.error(response.toString());
-                        }
-                        return response;
-                    }).build();
+                    .addInterceptor(OkHttpClientFactory::loggingInterceptor)
+                    .addInterceptor(OkHttpClientFactory::utf8EncodingInterceptor)
+                    .build();
         } catch (KeyManagementException | NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
