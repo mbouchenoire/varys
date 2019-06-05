c6c0236541bb2b237e7c0525ad72914596dcf75e
mbouchenoire <gitbouchenoire@gmail.com>
2019-06-05 21:23:34 +0200

    Add back missing Java file

---
+++ src/main/java/org/varys/common/Constants.java
@@ -0,0 +1,10 @@
+package org.varys.common;
+
+public final class Constants {
+
+    public static final String VARYS_FAVICON_URL = "https://i.imgur.com/Mlg9Atp.png";
+    public static final String GITLAB_FAVICON_URL = "https://gitlab.com/gitlab-org/gitlab-ce/raw/d031665a634e5d6fef980e837d6b227097850178/public/favicon.ico";
+    public static final String JENKINS_FAVICON_URL = "https://raw.githubusercontent.com/jenkinsci/jenkins/master/war/src/main/webapp/favicon.ico";
+
+    private Constants() {}
+}
