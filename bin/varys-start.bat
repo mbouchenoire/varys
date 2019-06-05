16bd6294104b2ea3f7f438d5cd4318ecf3e94434
mbouchenoire <gitbouchenoire@gmail.com>
2019-06-05 20:35:38 +0200

    Fix varys-start.bat script

--- bin/varys-start.bat
+++ bin/varys-start.bat
@@ -3,7 +3,7 @@
 SET config_file_path=%1
 
 IF "%1"=="" (
-    SET config_file_path="src\main\resources\config.json"
+    SET config_file_path="src\main\resources\config.yml"
 )
 
 java -cp target\varys-0.1.0-SNAPSHOT.jar org.varys.App %config_file_path%
